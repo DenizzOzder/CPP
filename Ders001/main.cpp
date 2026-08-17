8cb18230eb4c41e21b61d0d9fa3fe697dd48268d
Deniz Can Özder <candenizozder08@gmail.com>
2026-08-17 18:01:16 +0300

    data types and operators

--- Ders001/main.cpp
+++ Ders001/main.cpp
@@ -9,6 +9,26 @@ int main()
 	string isim; // Declaration | Tanımlama
 	isim = "Deniz"; // string tipinde bir değişken tanımladık ve değer atadık.
 	int yas = 25; // int tipinde bir değişken tanımladık ve değer atadık.
-	cout << isim << " " << yas << endl; // Ekrana yazdırma işlemi
-	return 0;
+	cout << isim << " " << yas << endl << endl; // Ekrana yazdırma işlemi
+	bool sinifiGectimi;
+	float ortalama = 95.5f; // float tipinde bir değişken tanımladık ve değer atadık.
+	double pi = 3.14159265358979323846; // double tipinde bir değişken tanımladık ve değer atadık.
+	cout << "Ortalama: " << ortalama << "\nPi: " << pi << endl; // Ekrana yazdırma işlemi
+	char notHarf = 65; // 65 => A harfine denk gelir.
+	int not1 = 90;
+	int not2 = 41;
+	if (ortalama >= 50)
+	{
+		sinifiGectimi = true; // bool tipinde bir değişken tanımladık ve değer atadık.
+		cout << "Sinifi gecti mi? " << sinifiGectimi << endl; // Ekrana yazdırma işlemi
+
+	}
+	else
+	{
+		sinifiGectimi = false; // bool tipinde bir değişken tanımladık ve değer atadık.
+		cout << "Sinifi gecti mi? " << sinifiGectimi << endl; // Ekrana yazdırma işlemi
+	}
+
+	cout << "Ortalama = " << (float) (not1 + not2) / 2<< endl;
+		return 0;
 }
