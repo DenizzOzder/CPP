928d7ef3dad4c8cabd4f13d0f0e8e45ed70f1f9f
Deniz Can Özder <candenizozder08@gmail.com>
2026-08-20 19:34:16 +0300

    Matris & Exercises

--- Ders006/main.cpp
+++ Ders006/main.cpp
@@ -40,19 +40,92 @@ int main() {
 		Bu notların toplamı ve Ortalamasını ekrana bastırın.
 
 	*/
-	const int SIZE = 4;
-	double notlar[SIZE];
-	double agirliklar[SIZE] = { 0.15, 0.25, 0.30, 0.40 };
-	double toplam = 0, ortalama = 0;
+	//const int SIZE = 4;
+	//double notlar[SIZE];
+	//double agirliklar[SIZE] = { 0.15, 0.15, 0.30, 0.40 };
+	//double toplam = 0, ortalama = 0;
 
-	for(int i = 0; i< SIZE; i++)
-	{ 
-		cout << "Lütfen " << i + 1 << ". sınav notunu giriniz: ";
-		cin >> notlar[i];
-		toplam += (notlar[i] * agirliklar[i]);
+	//for(int i = 0; i< SIZE; i++)
+	//{ 
+	//	cout << "Lütfen " << i + 1 << ". sınav notunu giriniz: ";
+	//	cin >> notlar[i];
+	//	toplam += (notlar[i] * agirliklar[i]);
+	//}
+	//ortalama = toplam / SIZE ;
+	//cout << "Ortalama: " << ortalama << endl;
+	//cout << "Toplam: " << toplam << endl;
+/* ---------------------------------------------------------------------------- */
+	// Multi Dimensional Arrays - Matris
+
+	int arr[2][3] = { {1, 2, 3}, {4, 5, 6} };
+	cout << arr[0][0] << endl; // 1
+	cout << "---------------------------------" << endl;
+
+	//Bütün diziyi yazdıralım.
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
 	}
-	ortalama = toplam / 4;
-	cout << "Ortalama: " << ortalama << endl;
-	cout << "Toplam: " << toplam << endl;
 
+	// Matris Exercise
+
+	/*
+		1. Kullanıcıdan 2 adet 3x2 Matris elemanlarını alacksınız
+		2. 2 Matrisin elemanlarını toplayıp 3x2 lik yeni bir matrise aktaracaksınız.
+	*/
+
+	/*int matris1[3][2], matris2[3][2], toplamMatris[3][2];
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << "Matris 1 için [" << i << "][" << j << "] elemanını giriniz: ";
+			cin >> matris1[i][j];
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << "Matris 2 için [" << i << "][" << j << "] elemanını giriniz: ";
+			cin >> matris2[i][j];
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			toplamMatris[i][j] = matris1[i][j] + matris2[i][j];
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << "Toplam Matris [" << i << "][" << j << "]: " << toplamMatris[i][j] << endl;
+		}
+	}*/
+
+	// Arrays with Functions
+
+	int dizi[] = { 3,9,12,5,10,1,3 };
+	diziDegistir(dizi, 7);
+	for (int i = 0;i < 7; i++)
+	{
+		cout << dizi[i] << " ";
+
+	}
+}
+
+// Out of Main Function
+void diziDegistir(int arr[], int size){
+	for (int i = 0; i < size;i++)
+	{
+		arr[i] += 1;
+	}
 }
